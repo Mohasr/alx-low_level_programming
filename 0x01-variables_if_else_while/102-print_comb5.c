69b31a15d0eb5e76311ffcdea54e6478e3d1ddb1
mohasr <mohaasrar@gmail.com>
2022-12-11 12:38:54 -0800

    all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,33 +11,36 @@ int i, j, k, l, d1, d2;
 i = j = k = l = 48;
 while (l < 58)
 {
-	j = 48;
-	while (j < 58)
+	k = 48;
+	while (k < 58)
 	{
-		i = 48;
-		while (i < 58)
+		j = 48;
+		while (j < 58)
 		{
-			d1 = (l * 10) + j;
-			d2 = (j * 10) + i;
-			if (d1 < d2)
+			i = 48;
+			while (i < 58)
 			{
-				putchar(l);
-				putchar(j);
-				putchar(' ');
-				putchar(j);
-				putchar(i);
-				if (l == 57 && j == 56 && j == 57 && i == 57)
-					break;
-				putchar(',');
-				putchar(' ');
+				d1 = (l * 10) + j;
+				d2 = (j * 10) + i;
+				if (d1 < d2)
+				{
+					putchar(l);
+					putchar(j);
+					putchar(' ');
+					putchar(j);
+					putchar(i);
+					if (l == 57 && j == 56 && j == 57 && i == 57)
+						break;
+					putchar(',');
+					putchar(' ');
+				}
+				i++;
 			}
-			i++;
+			j++;
 		}
 		j++;
 	}
-	j++;
-}
-l++;
+	l++;
 }
 putchar('\n');
 return (0);
