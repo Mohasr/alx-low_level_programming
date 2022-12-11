525e2c14d977dc057467924309c4cefe6f63a2a4
mohasr <mohaasrar@gmail.com>
2022-12-11 11:39:25 -0800

    prints all possible different combinations of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers between 012 to 789.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	i = 48;
+	j = 48;
+	k = 48;
+	while (j < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			k = 48;
+			while (k < 58)
+			{
+				if (j != i && j != k && i != k && j < i && i < k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i == 56 && j == 55 && k == 57)
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+				k++;
+			}
+			i++;
+		}
+		j++;
+	}
+	putchar('\n');
+	return (0);
+}
