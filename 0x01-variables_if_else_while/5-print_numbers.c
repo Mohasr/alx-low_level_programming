afdbeb52c6e1cfab8991b2ea60116b968eaed558
mohasr <mohaasrar@gmail.com>
2022-12-11 09:59:07 -0800

    print 0 to 9 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - function print numbers from 0 to 9.
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+
+	}
+	putchar('\n');
+	return (0);
+}
