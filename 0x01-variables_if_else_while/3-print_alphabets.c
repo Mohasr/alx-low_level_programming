5861214ba261e2621540999f06b58e50b2a0d1db
mohasr <mohaasrar@gmail.com>
2022-12-11 09:17:53 -0800

    print small and capital letter

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - function to print samll and capital letters
+ *
+ * Return: zero when successful
+ */
+int main(void)
+{
+	char alphabet[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alphabet[i]);
+	}
+	putchar('\n');
+	return (0);
+}
