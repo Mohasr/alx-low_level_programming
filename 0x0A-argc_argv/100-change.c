ca1a2b2f80a6b519d20627801f9f5ea7c6285745
mohasr <mohaasrar@gmail.com>
2023-01-06 11:54:37 -0800

    fix: 100-change file

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,7 +9,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int c, nc = 0;
+	int cents, n = 0;
 
 	if (argc == 1 || argc > 2)
 	{
@@ -17,20 +17,20 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	c = atoi(argv[1]);
+	cents = atoi(argv[1]);
 
-	while (c > 0)
+	while (cents > 0)
 	{
-		if (c >= 25)
-			c -= 25;
-		else if (c >= 10)
-			c -= 10;
-		else if (c >= 5)
-			c -= 5;
-		else if (c >= 2)
-			c -= 2;
-		else if (c >= 1)
-			c -= 1;
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else if (cents >= 1)
+			cents -= 1;
 		nc += 1;
 	}
 	printf("%d\n", nc);
